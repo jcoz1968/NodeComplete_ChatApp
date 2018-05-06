0c101113d3a3f7630c766f325228b048f7723114
Jeffery Cosby <jcoz1968@hotmail.com>
2018-05-06 16:16:17 -0500

    styles

--- public/js/index.js
+++ public/js/index.js
@@ -25,21 +25,16 @@ socket.on('newLocationMessage', function(message) {
     $('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Fred',
-    text: 'Hi'
-}, function(data) {
-    console.log(data);
-});
-
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
 
+    var messageEl = $('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageEl.val()
     }, function() {
-
+        messageEl.val('');
     });
 });
 
@@ -49,13 +44,17 @@ locationButton.on('click', function() {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported in your browser');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position);
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
         alert('unable to fetch location.');
+        locationButton.removeAttr('disabled').text('Send location');
     });
 });
