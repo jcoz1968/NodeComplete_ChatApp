80f292c0b69abd17fb038c6a6fef78023a9562a0
Jeffery Cosby <jcoz1968@hotmail.com>
2018-05-05 20:03:19 -0500

    newMessage, createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,17 @@ var socket = io();
 socket.on('connect', function()  {
     console.log('connected to server.');
     
-    socket.emit('createEmail', {
-        to: 'brennacosby@yahoo.com',
-        text: 'miss you!'
+    socket.emit('createMessage', {
+        from: 'Coz',
+        text: 'Hey, it worked'
     });
+
 });
 
 socket.on('disconnect', function() {
     console.log('disconnected from server.')
 });
 
-socket.on('newEmail', function(email) {
-    console.log('New Email', email)
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
 });
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newEmail', {
-      from: 'jcoz1968@gmail.com',
-      text: 'wassup',
-      createdAt: 123
+  socket.emit('newMessage', {
+    from: 'brenna',
+    text: 'hey dad!',
+    createdAt: 321123
   });
 
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
   });
 
   socket.on('disconnect', () => {
