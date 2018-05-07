d4f7b44c3d1b92182408aa91b5a23359320df3d5
Jeffery Cosby <jcoz1968@hotmail.com>
2018-05-06 19:24:28 -0500

    getuser, removeuser, tests

--- server/utils/users.js
+++ server/utils/users.js
@@ -7,22 +7,33 @@ class Users {
         this.users.push(user);
         return user;
     }
+
+    removeUser (id) {
+        var user = this.getUser(id);
+    
+        if (user) {
+          this.users = this.users.filter((user) => user.id !== id);
+        }
+    
+        return user;
+    }
+
+    getUser (id) {
+        // var user = this.users.filter((user) => user.id === id)[0];
+        // console.log(user);
+        return this.users.filter((user) => user.id === id)[0];
+      }
+
+    getUserList(room) {
+        var users = this.users.filter((user) => {
+            return user.room === room;
+        });
+        var namesArray = users.map((user) => {
+            return user.name;
+        });
+        return namesArray;
+    }
 }
 
 module.exports = { Users };
 
-// class Person {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.age = age;
-//     }
-
-//     getUserDescription() {
-//         return `${this.name} is ${this.age}`
-//     }
-
-// }
-
-// var me = new Person('brenna', 20);
-// var description = me.getUserDescription();
-// console.log(description);
